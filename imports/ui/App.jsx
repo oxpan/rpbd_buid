a20711415624aaf63225e5589256acb3fb64515b
OxPAN <kipir1@mail.ru>
2021-12-20 02:47:25 +0700

    начало работы над добавление адреса с привязкой

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 import { StreetForm } from './address/Street/StreetForm.jsx';
+import { AddressForm } from './address/AddressForm.jsx';
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
     {/* <Hello/> */}
     {/* <StreetForm/> */}
-    
 
+    <AddressForm/>
     {/* <Info/> */}
 
     
--- imports/ui/address/AddressForm.jsx
+++ imports/ui/address/AddressForm.jsx
@@ -1,4 +1,7 @@
+import { Mongo } from 'meteor/mongo'
 import React, { useState } from 'react';
+import { StreetForm } from './Street/StreetForm';
+
 
 
 export const AddressForm = () => {
@@ -13,12 +16,30 @@ export const AddressForm = () => {
         if (!apartment) return;
 
         Street_Collection.insert({
-            // idAdrees: 
+            // idAdrees: {_id: new Mongo.ObjectID(StreetForm)}, 
             home: home.trim(),
             apartment: apartment.trim()
         })
     }
     
-    
+    return (
+        <form className="address-form" onSubmit={onAddSumbit}>
+            {/* сюда нужно добавить выпадающий список с имеющими улицами */}
+            <input 
+                type={"number"}
+                placeholder='home'
+                value={home}
+                onChange={(e) => setHome(e.target.value)}
+                />
+            <input
+                type={"number"}
+                placeholder='apartment'
+                value={apartment}
+                onChange={(e) => setApartment(e.target.value)}
+                />
+
+            <button type="submit">Add stret</button>
+        </form>
+    )
 
 };

a019d21cf680f54565fd7cced4ff5b1c463dfb5e
OxPAN <kipir1@mail.ru>
2021-12-23 01:29:53 +0700

    добавление пустых кнопок

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-import { ListPersonShow } from './phonebook/ListPerson/ListPersonShow.jsx';
-import { PersonFind } from './phonebook/PersonFind.jsx';
+
 import { PersonForm } from './phonebook/PersonForm.jsx';
-import { menuInterface } from './ViewContrModel/menuInterface.jsx';
 
 export const App = () => (
   <div>
     <h1>PhoneBook:</h1>
-
-    {/* здесь будет поиск */}
-    {/* <Hello/> */}
-  
-    {/* <PersonFind/> */}
-    {/* <ListPersonShow/> */}
-
-    {/* <menuInterface/>
-     */}
-     {/* <menuInterface /> */}
     <PersonForm/>
-
-
-    {/* <Info/> */}
   </div>
 );
--- imports/ui/phonebook/ListAddress/ListAddress.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ListAddress = ({phonebook}) => {
-    return <li>{phonebook.street} {phonebook.home} {phonebook.apartment}</li>
-}

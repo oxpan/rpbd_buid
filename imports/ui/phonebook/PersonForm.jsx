f3cae2599033e31775f103400092324f36bdb2bc
OxPAN <kipir1@mail.ru>
2021-12-23 22:11:20 +0700

    Ересть тут была

--- imports/ui/phonebook/PersonForm.jsx
+++ imports/ui/phonebook/PersonForm.jsx
@@ -31,23 +31,119 @@ export const PersonForm = () => {
             firstname: firstName,
             fathername: fatherName
         })
-        // PhoneBook_Collection.addShard({
-        //     lastname: lastName,
-        //     firstname: firstName,
-        //     fathername: fatherName
-        // })
     }
     return (
         
-        <form className="person-insert-form" onSubmit={onAddSumbit}>
+        <div className="person-insert-form" >
+            
+        <div className='person-form' /*onSubmit={onAddSumbit}*/>
+            <span className='span1' >
+                {/* <div> */}
+                <form onSubmit={onAddSumbit}>
             <input 
                 type={"text"}
                 placeholder='FIO:'
-                value={buffer}
+                value={buffer.FIO}
                 onChange={(e) => setBuffer(e.target.value)}
                 />
-            <button type="submit">Add person</button>
-        </form>
+                
+            <input
+                // title='формат ввода [T X(XXX)XXX-XX-XX] T: mobile, work, home'
+                type={"text"}
+                placeholder='PhoneNumber'
+                value={buffer.phone}
+                onClick={(e) => setBuffer(e.target.value)}
+                />
+            
+            <input
+                type={"text"}
+                placeholder='Assress:'
+                value={buffer.address}
+                onClick={(e) => setBuffer(e.target.value)}
+                />
+                {/* </div> */}
+                
+                {/* <div> */}
+                <input name='radiob' type={"radio"} id='rad1'/>
+                <label htmlFor="rad1">find</label>
+                <input name='radiob' type={"radio"} id='rad2'/>
+                <label htmlFor="rad2">create</label>
+                
+                {/* </div> */}
+                <button type="submit">Исполнить</button>
+                 </form>
+            </span>
+           
+
+
+            <span className='span2'> 
+                <div>
+                    <button >ФИО список</button>
+                </div>
+                <div>
+                    <button >4 цифры</button>
+                </div>
+            </span>
+        </div>
+        <div>
+        <label>Данные адреса:</label>
+            <div>
+                
+            <input 
+                    type={"text"}
+                    placeholder='street:'
+                    // value={buffer}
+                    // onChange={(e) => setBuffer(e.target.value)}
+                    />
+                <input 
+                    type={"number"}
+                    placeholder='home:'
+                    // value={buffer}
+                    // onChange={(e) => setBuffer(e.target.value)}
+                    />
+                <input 
+                    type={"number"}
+                    placeholder='apartment:'
+                    // value={buffer}
+                    // onChange={(e) => setBuffer(e.target.value)}
+                    />
+            </div>
+            <label>Номера:</label>
+            <div>
+            <input 
+                type={"text"}
+                placeholder='mobilePhone:'
+                // value={buffer}
+                // onChange={(e) => setBuffer(e.target.value)}
+                />
+            </div>
+            <div>
+            <input 
+                type={"text"}
+                placeholder='workPhone:'
+                // value={buffer}
+                // onChange={(e) => setBuffer(e.target.value)}
+                />
+            </div>
+            <div>
+            <input 
+                type={"text"}
+                placeholder='homePhone:'
+                // value={buffer}
+                // onChange={(e) => setBuffer(e.target.value)}
+                />
+            </div>
+            
+        </div>
+
+        <div>
+        <button >обновить</button>
+        <button >удалить</button>
+        </div>
+        <div>
+            <p>tut vivod</p>
+        </div>
+</div>
     );
     
 };
--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-
+//\/ ЭТО НЕ ТРОГАТЬ!!! УДАЛЕНИЕ ПРИНОСИТ БОЛЬ!!!
 import { Type_of_phone_Collection } from '/imports/api/phonebook';
 
 Meteor.startup(() => {
+  //  ВНИМАНИЕ!!!! РАДИАКТИВНАЯ ЗОНА!!! ВСЕМ ПОКИНУТЬ ЭТОТ ФАЙЛ!!!! 
 
 });
